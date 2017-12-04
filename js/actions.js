7a9846378b04ffa963e6bfbef070e60ee9261e4c
SistemasCAE <sistemas_cae@cae3076.com>
2017-12-04 16:16:17 -0600

    primer guardado

---
+++ js/actions.js
@@ -0,0 +1,76 @@
+var fn = {
+	deviceready: function(){
+		//alert();
+		document.addEventListener("deviceready", fn.init/*this.init*/, false);
+	},
+	init: function(){
+		/*
+		 * En esta sección vamos a asociar
+		 * todos los eventos del "Click" al HTML
+		 */
+		 $("#botonIniciarSesion").tap(fn.iniciarSesion);
+	
+	},
+	iniciarSesion: function(){
+		var usuario = $("#usuarioSesion").val();
+		var password = $("#passwordSesion").val();
+		try{
+			if(usuario == ""){
+				throw new Error("Especifique su usuario");
+			}
+			if(password == ""){
+				throw new Error("Especifique su contraseña");
+			}
+			fn.enviarSesion(usuario, password);
+		}catch(error){
+			window.plugins.toast.show(error, 'short', 'center');
+		}
+	},
+	enviarSesion: function(usuario, password){
+		fn.quitarClases();
+		//alert("Enviando datos");
+		//alert("Nombre: "+nombreR+" Email: "+emailR+" Telefono: "+telefonoR+" Password: "+passwordR+" Foto: "+fotoR);
+		if(networkInfo.estaConectado() == false){
+			window.plugins.toast.show("No existe conexión a internet, revisela e intente de nuevo", 'long', 'center');
+			//alert("No existe conexión a internet, revisela e intente de nuevo");
+		}else{
+			$.ajax({
+				method: "POST",
+				url: "http://intranet.cae3076.com:50000/Vales_META-CAE/Recibe/compruebaSesion.php",
+				data: { 
+					usu: usuario,
+					pass: password
+				}
+			}).done(function(mensaje){
+				//alert("Datos enviados");
+				if(mensaje != "0"){
+					window.localStorage.setItem("nombreUsuario", usuario);
+					$("#usuario").html(usuario);
+					window.location.href="#inicio";
+				}else{
+					window.plugins.toast.show("Usuario/Contraseña invalido(s)", 'long', 'center');
+				}
+
+
+				//alert(mensaje);
+				//fn.sleep(3000);
+				//bcs.abrirCamara().delay( 3000 );
+			}).fail(function(error){
+				alert(error.status);
+				alert(error.message);
+				alert(error.responseText);
+			});
+		}
+		
+	}
+};
+/*
+ *Llamar al metodo Init en el navegador
+ */
+//fn.init();
+
+/*
+ *Llamar deviceready para compilar
+ */
+//
+fn.deviceready();
