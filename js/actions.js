a80cd414b7f84a1301abdb4cece629b897539271
SistemasCAE <sistemas_cae@cae3076.com>
2017-12-13 14:36:43 -0600

    JSON vales disponibles

--- js/actions.js
+++ js/actions.js
@@ -113,15 +113,19 @@ var fn = {
 		var colaborador= window.localStorage.getItem("nombreUsuario");
 		alert(colaborador);
 		alert("entre al if");
-		$.getJSON("http://intranet.cae3076.com:50000/Vales_META-CAE/php/json.php?opcion=2&colaborador="+colaborador, function(datos) {
+		var url = "http://intranet.cae3076.com:50000/Vales_META-CAE/php/json.php?";
+		$.getJSON(url, { 
+			opcion: 2,
+			colaborador: colaborador
+		}).done function(data){
 			alert("entre al json");
-			alert("Dato: " + datos["ID_VALE"]);
-			$.each(datos["ID_VALE"], function(i,id_vale) {
+			alert("Dato: " + datos);
+			$.each(datos.ID_VALE, function(i,id_vale) {
 				alert("ID VALE: " + id_vale);
 				$("#resultadoTabla").html("");
 				$("#resultadoTabla").append("<td>"+id_vale+"</td>");
 			});
-		});
+		}
 	}
 };
 /*
