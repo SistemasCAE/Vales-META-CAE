d6a4c58c3353326f7916118f21adfb44fec498b8
SistemasCAE <sistemas_cae@cae3076.com>
2017-12-19 18:16:05 -0600

    VISTA RESTAURANTES MAPA Y LOGOS

--- js/actions.js
+++ js/actions.js
@@ -151,7 +151,7 @@ var fn = {
 					center: new google.maps.LatLng(-34.397, 150.644),
 					mapTypeId: google.maps.MapTypeId.ROADMAP
 				  };
-				  map = new google.maps.Map(document.getElementById('map-canvas'+x),
+				  map = new google.maps.Map(document.getElementById('map-canvas'),
 					  mapOptions);
 			}
 			google.maps.event.addDomListener(window, 'load', initialize);*/
