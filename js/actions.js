23e4d6d6f854b36466477c9bdf9d78183aa5321c
SistemasCAE <sistemas_cae@cae3076.com>
2018-01-04 16:45:26 -0600

    VISTA RESTAURANTES MAPA Y LOGOS

--- js/actions.js
+++ js/actions.js
@@ -134,14 +134,11 @@ var fn = {
 		$.getJSON(url, { 
 			opcion: 3
 		}).done (function(data){
-			alert("entre al done");
 			var tamano = Object.keys(data).length;
 			$("#resultadoRestaurantes").html("");
-			alert("hay "+tamano+" restaurantes");
 			var tablaGenerada = "";
 			for(var x=0; x<tamano; x++)
 			{
-				alert("entre al for");
 				tablaGenerada += "<div>"+data[x]['NOMBRE']+"</div>";
 				//tablaGenerada += "<div>NOMBRE</div>";
 			}
